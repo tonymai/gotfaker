9f9fa213a7316d11f04f87afef78001638b0998d
Kevin C <kevin.ceballos@gmail.com>
2015-04-12 14:54:52 -0700

    debug

--- lib/gotfaker/character.rb
+++ lib/gotfaker/character.rb
@@ -4,8 +4,8 @@ require 'open-uri'
 module GOTFaker
 	class Character
 		def initialize(arg)
-			@noko = Nokogiri::HTML(open("http://awoiaf.westeros.org/index.php/List_of_characters"))
-			@names = @noko.search('#mw-content-text > ul >li> a:first-child').map{|name| name.inner_text}
+			noko = Nokogiri::HTML(open("http://awoiaf.westeros.org/index.php/List_of_characters"))
+			@names = noko.search('#mw-content-text > ul >li> a:first-child').map{|name| name.inner_text}
 		end
 
 		def self.random_full_name
